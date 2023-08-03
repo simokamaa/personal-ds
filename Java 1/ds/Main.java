593d0526332d0ba76961f739a903c332e251fb63
Simon Kamau <simonkamau690@gmail.com>
2023-08-03 13:24:35 +0300

    my ds
    ds update

---
+++ Java 1/ds/Main.java
@@ -0,0 +1,11 @@
+package ds;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello data structures");
+        for(int i=0; i<=10; i++)
+            for(int j=2; j<=3; j++)
+                System.out.println(i);
+
+    }
+}
